0186dacfd2515191df56f15069a57f45746aba8a
Hadi Alizadeh <haliz.dev@gmail.com>
2020-09-26 06:49:29 +0330

    created rendering mixin

--- src/mixins/mdRender.js
+++ src/mixins/mdRender.js
@@ -1,28 +1,30 @@
-import markdown from "markdown-it";
-import hljs from "highlight.js";
+import markdown from 'markdown-it'
+import hljs from 'highlight.js'
 
 export default {
-  data() {
+  data () {
     return {
-      apiMethod: {
-        title: '',
-        color: 'primary'
-      },
-      apiPath: '',
-      apiTitle: '',
-      apiRequests: '',
-      apiResponses: [],
-      parsedMd: '',
+      documentationForm : {
+        apiMethod: {
+          title: '',
+          color: 'primary'
+        },
+        apiPath: '',
+        apiTitle: '',
+        apiRequests: '',
+        apiResponses: [],
+        parsedMd: ''
+      }
     }
   },
   methods: {
-    mdRender() {
+    mdRender () {
       const md = new markdown({
         html: true,
         linkify: true,
         typographer: true,
         breaks: true,
-        highlight(str, lang) {
+        highlight (str, lang) {
           if (lang && hljs.getLanguage(lang)) {
             try {
               return `<pre class="hljs"><code>${
@@ -34,30 +36,38 @@ export default {
           return `<pre class="hljs"><code>${md.utils.escapeHtml(str)}</code></pre>`
         }
       })
-      this.parsedMd = md.render(this.synthesizeMd())
+      this.documentationForm.parsedMd = md.render(this.synthesizeMd())
     },
-    synthesizeMd() {
-      const apiTitle = '# '+this.apiTitle + '\n'
+    synthesizeMd () {
+      const apiTitle = `# ${this.documentationForm.apiTitle  }\n`
       const methodTag = this.renderMethodBlock()
       const responses = this.renderResponsesBlock()
       const requests = this.renderRequestsBlock()
-      return apiTitle + methodTag + requests + responses
+
+      //check for empty objects and avoid binding them
+      let data = ''
+      this.documentationForm.apiTitle !== '' ? data += apiTitle : 0
+      this.documentationForm.apiMethod.title !== '' ? data += methodTag : 0
+      this.documentationForm.apiRequests !== '' ? data += requests : 0
+      this.documentationForm.apiResponses.length > 0 ? data += responses : 0
+
+      return data
     },
     renderRequestsBlock () {
-      return '## Requests \n' + this.apiRequests
+      return `## Requests \n${  this.documentationForm.apiRequests}`
     },
     renderResponsesBlock () {
-      let responses = ''
-      this.apiResponses.forEach((val, index) => {
-        responses += `\n ## Responses \n <button type="button" disabled="disabled" class="vs-component mr-3 vs-button vs-button-${  val.color  } vs-button-filled">` +
+      let responses = '\n ## Responses \n '
+      this.documentationForm.apiResponses.forEach((val, index) => {
+        responses += `<button type="button" disabled="disabled" class="vs-component mr-3 vs-button vs-button-${  val.color  } vs-button-filled">` +
           `<span class="vs-button-text vs-button--text">${  val.code  }</span></button>` + `<span class="text-bold">${  val.title  }</span>` + `\n${
-            val.mdData  }\n`
+          val.mdData  }\n`
       })
       return responses
     },
-    renderMethodBlock() {
-      return `<button type="button" disabled="disabled" class="vs-component mr-3 vs-button vs-button-${this.apiMethod.color} vs-button-filled">` +
-        `<span class="vs-button-text vs-button--text">${this.apiMethod.title}</span></button>` + `<span class="text-bold">/${this.apiPath}</span> \n`
+    renderMethodBlock () {
+      return `<button type="button" disabled="disabled" class="vs-component mr-3 vs-button vs-button-${this.documentationForm.apiMethod.color} vs-button-filled">` +
+        `<span class="vs-button-text vs-button--text">${this.documentationForm.apiMethod.title}</span></button>` + `<span class="text-bold">/${this.documentationForm.apiPath}</span> \n`
     }
   }
 }
